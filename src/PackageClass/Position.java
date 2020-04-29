e5695bf074e6a68ae8c8c4e4c6123a7b6f987663
leo <leonard.almeras@gmail.com>
2020-04-29 12:25:09 +0200

    maze generation

---
+++ src/PackageClass/Position.java
@@ -0,0 +1,29 @@
+package PackageClass;
+
+public class Position {
+
+	private int coY;
+	private int coX;
+
+	public Position(int x ,int y)
+	{
+		this.setCoX(x);
+		this.setCoY(y);
+	}
+
+	public int getCoX() {
+		return coX;
+	}
+
+	public void setCoX(int coX) {
+		this.coX = coX;
+	}
+
+	public int getCoY() {
+		return coY;
+	}
+
+	public void setCoY(int coY) {
+		this.coY = coY;
+	}
+}
